6729f7b4231866bede5cf48a701f24c375e3d1c5
Shiwansh Tiwari <shiwanshtiwari281012@gmail.com>
2025-08-19 22:18:55 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,17 +1,18 @@
 class Solution {
     public int maxSubArray(int[] nums) {
+
         int sum=0;
-        int max=nums[0];
+        int maxsum=nums[0];
         if(nums.length==1)
         return nums[0];
 
         for(int val:nums)
         {
             sum+=val;
-            max=Math.max(sum,max);
+            maxsum=Math.max(sum,max);
             if(sum<0)
             sum=0;
         }
-        return max;
+        return maxsum;
     }
 }
