90aa9d581abf8ba9657974ae763d7ef73f3645a4
Shiwansh Tiwari <shiwanshtiwari281012@gmail.com>
2025-10-01 22:14:57 +0530

    Time: 12 ms (25.28%), Space: 44.9 MB (29.97%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.java
+++ 0224-basic-calculator/0224-basic-calculator.java
@@ -1,52 +1,48 @@
 class Solution {
     public int calculate(String s) {
-        int res=0;
-        int num=0;
-        int sign=1;
-        Stack<Integer>stk=new Stack<>();
-        int i=0;
-        while(i<s.length())
-        {   
-          char c=s.charAt(i);
-
-            if(c=='(')
-            {
-                stk.push(res);
-                stk.push(sign);
-                res=0;
-                sign=1;
-            }
-            
-            if(c==')')
-            {    
-                res=res+(num*sign);
-                num=0;
-                int presign=stk.pop();
-                int preres=stk.pop();
-                res=preres+(res*presign);
-            }
-            if(Character.isDigit(c))
-            {
+        int res = 0;
+        int num = 0;
+        int sign = 1;
+        Stack<Integer> stk = new Stack<>();
+        int i = 0;
+        while (i < s.length()) {
+            char c = s.charAt(i);
+            if (Character.isDigit(c)) {
                 num = num * 10 + (s.charAt(i) - '0');
             }
-            if(c=='+')
-            {
-                res=res+(num*sign);
-                num=0;
-                sign=1;
+
+            else if (c == '+') {
+                res = res + (num * sign);
+                num = 0;
+                sign = 1;
             }
 
-            if(c=='-')
-            {   
+            else if (c == '-') {
 
-                 res=res+(num*sign);
-                num=0;
-                sign=-1;
+                res = res + (num * sign);
+                num = 0;
+                sign = -1;
             }
-             i++;
+
+            else if (c == '(') {
+                stk.push(res);
+                stk.push(sign);
+                res = 0;
+                sign = 1;
+            }
+
+            else if (c == ')') {
+                res = res + (num * sign);
+                num = 0;
+                int presign = stk.pop();
+                int preres = stk.pop();
+                res = preres + (res * presign);
+            }
+
+            i++;
         }
-          
-          res=res+(num*sign);
+
+        res = res + (num * sign);
         return res;
     }
 }
