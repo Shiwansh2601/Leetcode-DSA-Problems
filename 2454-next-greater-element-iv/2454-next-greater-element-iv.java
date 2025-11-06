47c4d7808e5485467370dedfab286059ac8805de
Shiwansh Tiwari <shiwanshtiwari281012@gmail.com>
2025-11-06 17:12:36 +0530

    Time: 80 ms (51.16%), Space: 112.6 MB (5.23%) - LeetHub

--- 2454-next-greater-element-iv/2454-next-greater-element-iv.java
+++ 2454-next-greater-element-iv/2454-next-greater-element-iv.java
@@ -4,25 +4,22 @@ class Solution {
         Stack<Integer>stk1=new Stack<>();
         Stack<Integer>stk2=new Stack<>();
         Stack<Integer>temp=new Stack<>();
-        int[] res=new int[nums.length];
+        int[] res=new int[n];
         Arrays.fill(res,-1);
 
         for(int i=0;i<n;i++)
         {
             while(!stk2.isEmpty() && nums[stk2.peek()]<nums[i])
-            {
                 res[stk2.pop()]=nums[i];
-            }
+            
 
             while(!stk1.isEmpty() && nums[stk1.peek()]<nums[i] )
-            {
                 temp.push(stk1.pop());
-            }
+            
 
              while(!temp.isEmpty() )
-            {
                 stk2.push(temp.pop());
-            }
+
             stk1.push(i);
         }
 
