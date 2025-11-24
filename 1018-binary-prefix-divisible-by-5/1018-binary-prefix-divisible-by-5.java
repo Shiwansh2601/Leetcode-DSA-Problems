d8e61ef8f7235603e8386f6edeaed3184b5a2840
Shiwansh Tiwari <shiwanshtiwari281012@gmail.com>
2025-11-24 20:03:06 +0530

    Time: 4 ms (21.31%), Space: 47.6 MB (16.73%) - LeetHub

--- 1018-binary-prefix-divisible-by-5/1018-binary-prefix-divisible-by-5.java
+++ 1018-binary-prefix-divisible-by-5/1018-binary-prefix-divisible-by-5.java
@@ -4,9 +4,9 @@ class Solution {
         List<Boolean>res=new ArrayList<>();
         for(int i=0;i<nums.length;i++)
         {
-            x=(2*x)+nums[i];
+            x=((2*x)+nums[i])%5;
 
-            if(x%5==0)
+            if(x==0)
               res.add(true);
             else
               res.add(false);
