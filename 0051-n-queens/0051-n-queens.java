c56a2beefa44b6ab8fb2e5360994b94fb5d80102
Shiwansh Tiwari <shiwanshtiwari281012@gmail.com>
2025-11-24 23:39:40 +0530

    Time: 2 ms (84.19%), Space: 46.7 MB (13.22%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.java
@@ -0,0 +1,79 @@
+class Solution {
+
+    public void solve(int col,char[][] board,List<List<String>>res,int n)
+    {
+        if(col==n)
+        {
+            List<String>one=new ArrayList<>();
+            for(int i=0;i<n;i++)
+            {
+                one.add(new String(board[i]));
+            }
+             res.add(one);
+            return;
+        }
+
+        for(int row=0;row<n;row++)
+        {
+            if(issafe(col,row,board,n))
+            {
+                board[row][col]='Q';
+                solve(col+1,board,res,n);
+                board[row][col]='.';   // Backtrack
+            }
+        }
+    }
+    public boolean issafe(int col,int row,char[][] board,int n)
+    {
+
+        int c=col;
+        int r=row;
+        while(c>=0)
+        {
+            if(board[r][c]=='Q')
+            return false;
+
+            c--;
+        }
+
+        c=col;
+        r=row;
+        while(c>=0 && r>=0)
+        {
+            if(board[r][c]=='Q')
+            return false;
+            c--;
+            r--;
+        }
+
+         c=col;
+        r=row;
+        while(c>=0 && r<n)
+        {
+            if(board[r][c]=='Q')
+            return false;
+            c--;
+            r++;
+        }
+        
+        return true;
+
+    }
+    public List<List<String>> solveNQueens(int n) {
+        char[][] board=new char[n][n];
+
+        for(int i=0;i<n;i++)
+        {
+            Arrays.fill(board[i],'.'); // imp
+        }
+
+        List<List<String>>res=new ArrayList<>();
+        solve(0,board,res,n);
+
+
+        return res;
+
+
+
+    }
+}
