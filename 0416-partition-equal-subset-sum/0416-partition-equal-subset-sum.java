c4888ced71faad63bad5fb06735659eee0284aad
Shiwansh Tiwari <shiwanshtiwari281012@gmail.com>
2026-01-23 15:32:17 +0530

    Time: 73 ms (34.33%), Space: 47.5 MB (64.6%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,48 +1,47 @@
 class Solution {
 
-    // Memoization DP
+    // Tabulation DP
     public boolean canPartition(int[] nums) {
         return solve(nums);
     }
 
-    public boolean solve(int[] arr)
-    {
-        int sum=0;
-      for(int i=0;i<arr.length;i++)
-      {
-          sum+=arr[i];
-      }
-      
-      if(sum%2!=0)
-      return false;
-      else
-      {
-         int[][] dp=new int[arr.length+1][sum/2+1];
-         for(int i=0;i<=arr.length;i++)
-         {
-            Arrays.fill(dp[i],-1);
-         }
+    public boolean solve(int[] arr) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
 
-         return subsetsum(arr,arr.length,sum/2,dp);
-      }
-      
+        if (sum % 2 != 0)
+            return false;
+        else {
+            boolean[][] dp = new boolean[arr.length + 1][sum / 2 + 1];
+            for (int i = 0; i <= arr.length; i++) {
+                for (int j = 0; j <= sum / 2; j++) { // initilization
+                    if (j > 0 && i == 0)
+                        dp[i][j] = false;
+
+                    if (j == 0)
+                        dp[i][j] = true;
+                }
+            }
+
+            return subsetsum(arr, arr.length, sum / 2, dp);
+        }
+
     }
-    public boolean subsetsum(int arr[],int n, int sum,int[][] dp) {
-        
-        if(sum>0 && n==0)
-        return false;
-        
-        if(sum==0)
-        return true;
-        
-        if(dp[n][sum]!=-1)
-        return dp[n][sum]==1;
 
-        if(arr[n-1]<=sum)
-        dp[n][sum]=subsetsum(arr,n-1,sum-arr[n-1],dp) ||subsetsum(arr,n-1,sum,dp)?1:0;
-        else
-         dp[n][sum]=subsetsum(arr,n-1,sum,dp)?1:0;
+    public boolean subsetsum(int arr[], int n, int sum, boolean[][] dp) {
 
-          return dp[n][sum]==1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= sum; j++) {
+                if (arr[i - 1] <= j)
+                    dp[i][j] = dp[i - 1][j - arr[i - 1]] || dp[i - 1][j];
+                else
+                    dp[i][j] = dp[i - 1][j];
+
+            }
+        }
+
+        return dp[n][sum];
     }
 }
