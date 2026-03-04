7fa89763945baaf50481060e1af1680ebd01f688
Shiwansh Tiwari <shiwanshtiwari281012@gmail.com>
2026-03-04 21:12:41 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,37 +1,26 @@
 class Solution {
     public int rob(int[] nums) {
-
-    
-        int n=nums.length;
-        if(n==1)
-        return nums[0];
-        int[] dp1=new int[n+1];
-        int[] dp2=new int[n+1];
-        Arrays.fill(dp1,-1);
-        Arrays.fill(dp2,-1);
-        return Math.max(solve1(0,n-1,nums,dp1),solve2(1,n,nums,dp2));
+        int n =nums.length;
+        if(n==1){
+            return nums[0];
+        }
+       int[]dp1 = new int [n+1];
+       int[]dp2 = new int[n+1];
+       Arrays.fill(dp1,-1);
+       Arrays.fill(dp2,-1);
+       int n1 =solve(nums,dp1,n-2,0);
+       int n2= solve(nums,dp2,n-1,1);
+        return Math.max(n1,n2);
     }
-    public int solve1(int i,int end,int[] nums,int[] dp)
-    {   
-        if(i>=end)
-         return 0;
+    public int solve(int[]arr, int[]dp,int i, int j){
+         if (i < j) return 0;
+        if(dp[i]!=-1){
+            return dp[i];
+        }
+        
+        int pick = arr[i]+solve(arr,dp,i-2,j);
+        int npick = solve(arr,dp,i-1,j);
+        return dp[i]=Math.max(pick,npick);
 
-        if(dp[i]!=-1)
-        return dp[i];
-
-        return dp[i]=Math.max(nums[i]+solve1(i+2,end,nums,dp),solve1(i+1,end,nums,dp));
-    }
-
-    public int solve2(int i,int end,int[] nums,int[] dp)
-    {   
-        if(i>=end)
-         return 0;
-
-       
-
-        if(dp[i]!=-1)
-        return dp[i];
-
-          return dp[i]=Math.max(nums[i]+solve1(i+2,end,nums,dp),solve1(i+1,end,nums,dp));
     }
 }
