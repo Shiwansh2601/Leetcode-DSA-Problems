794cbc3c5123127fd3b20f68c9d381cd6db5c3a5
Shiwansh Tiwari <shiwanshtiwari281012@gmail.com>
2026-03-05 15:41:25 +0530

    Time: 10 ms (8.78%), Space: 48.7 MB (5.55%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.java
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.java
@@ -4,30 +4,28 @@ class Solution {
         int n=graph.length;
         List<Integer>list=new ArrayList<>();
         list.add(0);
-        boolean[] vis=new boolean[n];
-        dfs(graph,list,vis,0,n-1);
+        Queue<List<Integer>>q=new LinkedList<>();
+        q.add(list);
+        while(!q.isEmpty()){
+
+            List<Integer>curr=q.poll();
+            int node=curr.get(curr.size()-1);
+            if(node==n-1)
+            {
+                ans.add(curr);
+                continue;
+            }
+
+            for(int e:graph[node])
+            {
+                List<Integer>newpath=new ArrayList<>(curr);
+                newpath.add(e);
+                q.add(newpath);
+            }
+        }
         return ans;
 
     }
 
-    public void dfs(int[][]graph,List<Integer>list,boolean[] vis,int curr,int dest){
-        if(curr==dest)
-        {
-            ans.add(new ArrayList<>(list));
-            return;
-        }
-
-        for(int i=0;i<graph[curr].length;i++)
-        {
-             int x=graph[curr][i];
-             if(vis[x]==false)
-             {
-                vis[x]=true;
-                list.add(x);
-                dfs(graph,list,vis,x,dest);
-                vis[x]=false;
-                list.remove(list.size()-1);
-             }
-        }
-    }
+    
 }
