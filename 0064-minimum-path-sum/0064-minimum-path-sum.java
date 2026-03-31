561ad41e4c18a5eae1470d43681806fa8e33bd8c
Shiwansh Tiwari <shiwanshtiwari281012@gmail.com>
2026-03-31 22:36:03 +0530

    Time: 4 ms (63.55%), Space: 50 MB (92.05%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -2,35 +2,36 @@ class Solution {
     public int minPathSum(int[][] grid) {
         int m=grid.length;
         int n=grid[0].length;
+         int max=Integer.MAX_VALUE;
+         int[][] dp=new int[m][n];
+         dp[0][0]=grid[0][0];  //intialization
+         for(int i=0;i<m;i++)
+         {
+            for(int j=0;j<n;j++)
+            {
+                if(i>0 && j>0)
+                {
+                    dp[i][j]=grid[i][j]+Math.min(dp[i][j-1],dp[i-1][j]);
+                }
+                else
+                {
+                    if(i==0 && j==0)
+                    continue;
 
-         int[][] dp=new int[m+1][n+1];
-        for(int i=0;i<=m;i++)
-        {
-            Arrays.fill(dp[i],-1);
-        }
-        return solve(grid,dp,m,n,0,0);
+                    if(i==0)
+                    {
+                        dp[i][j]=grid[i][j]+Math.min(dp[i][j-1],max);
+                    }
+                    else
+                    {
+                        dp[i][j]=grid[i][j]+Math.min(dp[i-1][j],max);
+                    }
+                }
+            }
+         }
+
+         return dp[m-1][n-1];
+      
     }
 
-     public int solve(int[][] mat,int[][] dp,int m,int n,int i,int j)
-    {
-        if(i==m-1 && j==n-1)
-        {
-            return mat[i][j];
-        }
-        
-        if(i<0 ||i>=m  ||j<0 || j>=n)
-        {
-            return Integer.MAX_VALUE;
-        }
-        
-       if(dp[i][j]!=-1)
-       return dp[i][j];
-       
-       
-        
-        int down=solve(mat,dp,m,n,i+1,j);
-        int right=solve(mat,dp,m,n,i,j+1);
-        
-        return dp[i][j]=Math.min(down,right)+mat[i][j];
-    }
 }
