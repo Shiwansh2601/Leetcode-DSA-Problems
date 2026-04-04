a7a25e07f9de515950909c0d47a91923f82e9cde
Shiwansh Tiwari <shiwanshtiwari281012@gmail.com>
2026-04-05 02:21:01 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,38 +1,30 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int n=prices.length;
-        int[][][] dp=new int[n][2][3];
+         int n = prices.length;
 
-        for(int i=n-1;i>=0;i--)
-        {
-            for(int j=0;j<2;j++)
-            {
-                Arrays.fill(dp[i][j],-1);
+        int[][][] dp = new int[n + 1][2][3];
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                for (int cap = 1; cap <= 2; cap++) {
+
+                    if (buy == 1) {
+                        dp[i][buy][cap] = Math.max(
+                            -prices[i] + dp[i + 1][0][cap],
+                            dp[i + 1][1][cap]
+                        );
+                    } else {
+                        dp[i][buy][cap] = Math.max(
+                            prices[i] + dp[i + 1][1][cap - 1],
+                            dp[i + 1][0][cap]
+                        );
+                    }
+
+                }
             }
         }
 
-        return solve(0,0,2,prices,dp);
+        return dp[0][1][2];
     }
 
-
-    public int solve(int i,int f,int cap,int[] arr,int[][][] dp)
-    {
-        if(i==arr.length|| cap==0)
-        return 0;
-
-        if(dp[i][f][cap]!=-1)
-        return dp[i][f][cap];
-        
-        int pro=0;
-        if(f==0)
-        {
-             pro= Math.max(-arr[i]+solve(i+1,1,cap,arr,dp),solve(i+1,0,cap,arr,dp));
-        }
-        else
-        {
-            pro= Math.max(arr[i]+solve(i+1,0,cap-1,arr,dp),solve(i+1,1,cap,arr,dp));
-        }
-
-        return dp[i][f][cap]=pro;
-    }
 }
