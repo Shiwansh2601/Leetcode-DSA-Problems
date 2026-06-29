0a933abbf033eb60bfd1665446c5dcd31ac4074a
Shiwansh Tiwari <shiwanshtiwari281012@gmail.com>
2026-06-29 10:20:00 +0530

    Time: 4 ms (45.35%), Space: 54.8 MB (5.71%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,35 +1,48 @@
 class Solution {
-   
-    public void dfs(int i,int j,boolean[][] vis,char[][] grid)
-    {
-          if(i<0||j<0||i>=grid.length||j>=grid[0].length||grid[i][j]=='0'||vis[i][j])
-          return;
-
-          vis[i][j]=true;
-          dfs(i+1,j,vis,grid);
-          dfs(i,j+1,vis,grid);
-          dfs(i-1,j,vis,grid);
-          dfs(i,j-1,vis,grid);
-    }
-    
     public int numIslands(char[][] grid) {
         int n=grid.length;
         int m=grid[0].length;
-        int res=0;
-        boolean[][] vis=new boolean[n][m];
+        int count=0;
+
+       boolean[][] vis=new boolean[n][m];
+
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-                if(!vis[i][j] &&grid[i][j]=='1')
-                {
-                    res++;
-                    dfs(i,j,vis,grid);
-                }
+               if(!vis[i][j] && grid[i][j]!='0')
+              {
+                 dfs(i,j,vis,grid);
+                 count++;
+              }
+            
             }
         }
+       
+       return count;
+        
+    }
 
-    return res;
+    public void dfs(int i,int j,boolean[][] vis,char[][] grid)
+    {
+        int  m=grid.length;
+        int n=grid[0].length;
+
+        if(i<0 || i>=m||j<0||j>=n || vis[i][j] || grid[i][j]=='0')
+        return;
+
+        grid[i][j]='0';
+        vis[i][j]=true;
+
+        int[] dx={0,1,0,-1};
+        int[] dy={1,0,-1,0};
+
+        for(int k=0;k<4;k++)
+        {
+            dfs(i+dx[k],j+dy[k],vis,grid);
+        }
+
+        
 
     }
 }
