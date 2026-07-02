ac0df698ca0842c1a867fa19bfd998ed34261048
Shiwansh Tiwari <shiwanshtiwari281012@gmail.com>
2026-07-03 02:03:37 +0530

    Time: 1 ms (90.71%), Space: 47.3 MB (53.57%) - LeetHub

--- 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.java
+++ 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.java
@@ -1,103 +1,28 @@
 class Solution {
     public int findUnsortedSubarray(int[] nums) {
-        
-        int l=0;
-        int r=nums.length-1;
-
-            int s=posL(l,r,nums);
-            int e=posR(l,r,nums);
-           
-         int ans=0;
-        while(s<e  && s>=0 && e<nums.length)
-        {
-            
-            int[] pair=findminmax(s,e,nums);
-            int min=pair[0];
-            int max=pair[1];
-
-            if(check(min,max,s,e,nums)==0)
-            {
-                return e-s+1;
-            }
-            else if(check(min,max,s,e,nums)==1)
-            {
-                 s--;
-            }
-            else
-            {
-                e++;
-            }
-
-            
+     int max = Integer.MIN_VALUE;
+     int right = -1;
+     for(int i = 0 ; i<nums.length;i++){
+        if(nums[i]>max){
+            max = nums[i];
         }
-
-        return ans;
-    }
-
-    public int posL(int l,int r,int[] nums)
-    {
-        
-            while(l<r && nums[l]<=nums[l+1])
-            {
-                l++;
-            }
-
-            return l;
-    }
-
-     public int posR(int l,int r,int[] nums)
-    {
-    
-
-            while(l<r && nums[r]>=nums[r-1])
-            {
-                r--;
-            }
-            return r;
-    }
-
-    public int[] findminmax(int i,int n,int[] nums)
-    {
-            int max=-100001;
-            int min=100001;
-            
-            while(i<=n)
-            {
-                if(nums[i]>max)
-                max=nums[i];
-
-                if(nums[i]<min)
-                min=nums[i];
-
-                i++;
-            }
-           
-
-            int[] ans={min,max};
-
-            return ans;
-    }
-
-    public int check(int min,int max,int s,int e,int[] nums)
-    {
-      
-
-        int i=0;
-        while(i<s && nums[i]<=min)
-        {
-            i++;
+        if(nums[i]<max){
+            right=i;
         }
-        int j=e+1;
-
-        while(j<nums.length && nums[j]>=max)
-        j++;
-
-        if(i==s && j==nums.length)
+     }
+     if(right==-1){
         return 0;
-        else if(i==s)
-        return 2;
-
-        return 1;
-
+     }
+     int min = Integer.MAX_VALUE;
+     int left = 0;
+     for(int i = nums.length-1;i>=0;i--){
+        if(nums[i]<min){
+            min=nums[i];
+        }
+        if(nums[i]>min){
+            left = i;
+        }
+     }
+     return (right-left)+1;
     }
 }
