5c65f97403049791c0a437788b768e67c3374a41
Shiwansh Tiwari <shiwanshtiwari281012@gmail.com>
2026-08-06 22:32:51 +0530

    Time: 674 ms (5.02%), Space: 42.8 MB (94.13%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,38 +1,29 @@
 class Solution {
+    int count=0;
     public int findTargetSumWays(int[] nums, int target) {
-        int total=0;
-        int n=nums.length;
-        for(int i=0;i<n;i++)
-        {
-            total+=nums[i];
-        }
-
-        int sum=(target+total);
-
-        if(sum%2!=0 ||Math.abs(target)>total)
-        return 0;
-       
-       int[][] dp=new int[n+1][sum/2+1];
-       dp[0][0]=1;
-
-        return issubsetexist(nums,n,sum/2,dp);
+        
+        solve(0,0,nums,target);
+        return count;
     }
 
-    public int issubsetexist(int[] nums,int n,int sum,int[][] dp)
+    public void solve(int sum,int i,int[] nums,int t)
     {
-        for(int i=1;i<=n;i++)
+        if(i==nums.length)
         {
-            for(int j=0;j<=sum;j++)
-            {
-                if(nums[i-1]<=j)
-                {
-                    dp[i][j]=dp[i-1][j-nums[i-1]]+dp[i-1][j];
-                }
-                else
-                dp[i][j]=dp[i-1][j];
-            }
+            if(sum==t)
+            count++;
+
+            return;
         }
 
-         return dp[n][sum];
+
+        sum+=nums[i];
+        solve(sum,i+1,nums,t);
+        sum-=nums[i];
+
+
+        sum-=nums[i];
+        solve(sum,i+1,nums,t);
+        sum+=nums[i];
     }
 }
