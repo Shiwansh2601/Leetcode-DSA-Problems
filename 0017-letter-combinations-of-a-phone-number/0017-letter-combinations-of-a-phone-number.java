bb55069dd7beadd60a72c56eeabcce5695fba128
Shiwansh Tiwari <shiwanshtiwari281012@gmail.com>
2026-08-06 21:44:10 +0530

    Time: 0 ms (100%), Space: 43.5 MB (64.3%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,66 +1,38 @@
 class Solution {
-    List<String>res=new ArrayList<>();
-    
+    List<String> ans = new ArrayList<>();
+    HashMap<Character, String> map = new HashMap<>();
+
     public List<String> letterCombinations(String digits) {
-        backtrack(digits,0,new StringBuilder());
-        return res;
-        
+        map.put('2', "abc");
+        map.put('3', "def");
+        map.put('4', "ghi");
+        map.put('5', "jkl");
+        map.put('6', "mno");
+        map.put('7', "pqrs");
+        map.put('8', "tuv");
+        map.put('9', "wxyz");
+
+        solve(0, digits, new StringBuilder());
+        return ans;
+
     }
-    public void backtrack(String digits,int indx,StringBuilder str){
-        //base case
-        if(indx==digits.length())
-        {
-            res.add(str.toString());
+
+    public void solve(int i, String d, StringBuilder str) {
+        if (str.length() == d.length()) {
+            ans.add(str.toString());
             return;
         }
-        
-        String letters=Alphabet(digits.charAt(indx));
-        
-        for(char ch:letters.toCharArray())
-        {
-            str.append(ch);
-            backtrack(digits,indx+1,str);
-            str.deleteCharAt(str.length()-1);
+
+        char ch = d.charAt(i);
+        String word = map.get(ch);
+
+        for (int j = 0; j < word.length(); j++) {
+            char c = word.charAt(j);
+            str.append(c);
+            solve(i + 1, d, str);
+            str.deleteCharAt(str.length() - 1);
         }
-        
-        
-        
+
     }
-    public String Alphabet(char ch)
-    {
-        if(ch == '2')
-        {
-            return "abc";
-        }
-        if(ch == '3')
-        {
-            return "def";
-        }
-        if(ch == '4')
-        {
-            return "ghi";
-        }
-        if(ch == '5')
-        {
-            return "jkl";
-        }
-        if(ch == '6')
-        {
-            return "mno";
-        }
-        if(ch == '7')
-        {
-            return "pqrs";
-        }
-        if(ch == '8')
-        {
-            return "tuv";
-        }
-        if(ch == '9')
-        {
-            return "wxyz";
-        }
-    
-    return "";
-}
+
 }
