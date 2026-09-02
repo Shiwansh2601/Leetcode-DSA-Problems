7f243f0f0edb89ed4e4306c2b33c5f5525db40a6
Shiwansh Tiwari <shiwanshtiwari281012@gmail.com>
2026-09-02 18:58:35 +0530

    Time: 2 ms (99.03%), Space: 47 MB (13.33%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -1,43 +1,39 @@
 class Solution {
-
-    List<List<Integer>> res = new ArrayList<>();
-    List<Integer> current = new ArrayList<>();
-
-    public List<List<Integer>> permuteUnique(int[] nums) {
-
-        Arrays.sort(nums);
-
-        boolean[] used = new boolean[nums.length];
-
-        solve(nums, used);
-
-        return res;
-    }
-
-    void solve(int[] nums, boolean[] used) {
-
-        if (current.size() == nums.length) {
-            res.add(new ArrayList<>(current));
+    List<List<Integer>>res=new ArrayList<>();
+    public void solve(int[] arr,int indx)
+    {
+        if(indx==arr.length)
+        {
+           List<Integer> list = new ArrayList<>();
+             for (int x : arr) list.add(x);
+            res.add(list);
             return;
         }
+        
+        HashSet<Integer>set=new HashSet<>();
+        for(int i=indx;i<arr.length;i++)
+        {
+            if(set.contains(arr[i]))
+            continue;
 
-        for (int i = 0; i < nums.length; i++) {
+            set.add(arr[i]);
+            
+            swap(arr,i,indx);
+            solve(arr,indx+1);
+            swap(arr,i,indx);
 
-            // Already used in current permutation
-            if (used[i])
-                continue;
-
-            // Skip duplicate branches
-            if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1])
-                continue;
-
-            used[i] = true;
-            current.add(nums[i]);
-
-            solve(nums, used);
-
-            current.remove(current.size() - 1);
-            used[i] = false;
         }
+    }
+
+    public void swap(int arr[],int i,int j)
+    {
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        solve(nums,0);
+
+        return res;
     }
 }
