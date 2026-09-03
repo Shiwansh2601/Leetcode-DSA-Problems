0e484618fce412b2c1ebdb2b021a2354e51d26a3
Shiwansh Tiwari <shiwanshtiwari281012@gmail.com>
2026-09-03 09:17:28 +0530

    Time: 0 ms (100%), Space: 42.8 MB (37.84%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -12,9 +12,6 @@ class Solution {
 
         for(int i=2;i<n;i++)
         {
-            int take=nums[i]+a;
-            int ntake=b;
-
             int c=Math.max(nums[i]+a,b);
 
             a=b;
@@ -23,19 +20,5 @@ class Solution {
 
         return b;
        
-    }
-
-    public int solve(int n,int[] arr,int[] dp)
-    {
-        if(n<0)return 0;
-
-        if(n==0)return arr[n];
-
-        if(dp[n]!=-1)return dp[n];
-
-        int take= arr[n]+solve(n-2,arr,dp);
-        int ntake=solve(n-1,arr,dp);
-
-        return dp[n]=Math.max(take,ntake);
     }
 }
