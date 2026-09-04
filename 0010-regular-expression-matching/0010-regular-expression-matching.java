5de4a365e0703dfce6abae215d7c656aafe901d9
Shiwansh Tiwari <shiwanshtiwari281012@gmail.com>
2026-09-05 03:51:04 +0530

    Time: 1 ms (99.99%), Space: 43.6 MB (33.36%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.java
+++ 0010-regular-expression-matching/0010-regular-expression-matching.java
@@ -1,6 +1,8 @@
 class Solution {
+    Boolean[][] dp;
     public boolean isMatch(String s, String p) {
-        
+        dp=new Boolean[s.length()+1][p.length()+1];
+
         return solve(0,0,s,p);
     }
 
@@ -10,6 +12,8 @@ class Solution {
         {
             return i==s.length();
         }
+
+        if(dp[i][j] !=null) return dp[i][j];
 
         boolean firstchar=false;
 
@@ -24,9 +28,9 @@ class Solution {
 
             boolean take= firstchar && solve(i+1,j,s,p);
 
-            return take||skip;
+            return dp[i][j]= take||skip;
         }
 
-        return firstchar && solve(i+1,j+1,s,p);
+        return dp[i][j]= firstchar && solve(i+1,j+1,s,p);
     }
 }
