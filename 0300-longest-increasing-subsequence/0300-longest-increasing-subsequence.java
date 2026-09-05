b624f6b328219611f31e3c57017d0d13dbd0a08d
Shiwansh Tiwari <shiwanshtiwari281012@gmail.com>
2026-09-05 09:17:12 +0530

    Time: 37 ms (74.48%), Space: 45.9 MB (92.53%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,52 +1,32 @@
-import java.util.*;
-
 class Solution {
-
-    int[] nums;
-    int[][] memo;
 
     public int lengthOfLIS(int[] nums) {
 
-        this.nums = nums;
-
         int n = nums.length;
 
-        // prev = -1 is represented using n
-        memo = new int[n][n + 1];
+        int[] dp = new int[n];
 
-        for (int[] row : memo) {
-            Arrays.fill(row, -1);
+        Arrays.fill(dp, 1);
+
+        int ans = 1;
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < i; j++) {
+
+                if (nums[j] < nums[i]) {
+
+                    dp[i] =
+                        Math.max(
+                            dp[i],
+                            dp[j] + 1
+                        );
+                }
+            }
+
+            ans = Math.max(ans, dp[i]);
         }
 
-        return solve(0, n);
-    }
-
-    private int solve(int i, int prev) {
-
-        if (i == nums.length) {
-            return 0;
-        }
-
-        if (memo[i][prev] != -1) {
-            return memo[i][prev];
-        }
-
-        // Skip current element
-        int skip = solve(i + 1, prev);
-
-        // Take current element
-        int take = 0;
-
-        if (
-            prev == nums.length ||
-            nums[i] > nums[prev]
-        ) {
-
-            take =
-                1 + solve(i + 1, i);
-        }
-
-        return memo[i][prev] =
-            Math.max(take, skip);
+        return ans;
     }
 }
